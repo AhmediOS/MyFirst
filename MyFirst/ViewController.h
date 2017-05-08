43c652be85c5839988136c214938a7b916ec0de2
Admin <admin@admins-mac.local>
2017-05-08 17:51:23 +0530

    Initial Commit

---
+++ MyFirst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirst
+//
+//  Created by Admin on 08/05/17.
+//  Copyright © 2017 Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFirst/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyFirst
+//
+//  Created by Admin on 08/05/17.
+//  Copyright © 2017 Ahmed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
